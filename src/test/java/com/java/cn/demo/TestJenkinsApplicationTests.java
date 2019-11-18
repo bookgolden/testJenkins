8c1d677b704b2e55c7179be1b54bdaad38d5563d
bob <bookgolden@126.com>
2019-11-18 23:10:41 +0800

    init 0

---
+++ src/test/java/com/java/cn/demo/TestJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.cn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
