cd761346f4bed678705081a7efa004126468208f
bookgolden <bookgolden@126.com>
2021-02-19 16:21:57 +0800

    tst

--- src/main/java/com/java/cn/controller/World.java
+++ src/main/java/com/java/cn/controller/World.java
@@ -5,7 +5,6 @@ public class World {
 
     public static void main(String[] args) {
         System.out.println("hello world ~ ");
-        System.out.println("hello world ~ ");
-        System.out.println("hello world ~ ");
+        System.out.println("hello cs ~ ");
     }
 }
